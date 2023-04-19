221f5ad394677526f31377ddceb852a9e44617db
Rievaii <46780629+Rievaii@users.noreply.github.com>
2023-04-19 19:07:27 +0300

    Field Initial

---
+++ ConsoleSnake/ConsoleSnake/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleSnake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Field = new List<string>();
+
+            for (int i = 0; i < 25; i++)
+            {
+                if(i % 5 == 0)
+                {
+                    Field.Add("\n");
+                }
+                Field.Add(new FieldCell().value);
+            }
+            foreach (var item in Field)
+            {
+                
+                Console.Write(item);
+            }
+        }
+    }
+}
